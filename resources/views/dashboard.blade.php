ed8add5e978af4cf7788666cf0e899716830f672
ttnhan2121 <ttnhan1212@hotmail.com>
2024-05-20 02:41:51 +0700

    123

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,21 +1,29 @@
 @extends('home')
 @section('content')
-
         <div class="row" style="height: 50px">
             <div class="col d-flex justify-content-end align-item-center">
                 <span class="material-symbols-outlined fs-1 p-2" >
                 account_circle
                 </span>
-                <label class="toggle-switch">
-                    <input type="checkbox" id="check123">
-                    <div class="toggle-switch-background">
-                        <div class="toggle-switch-handle"></div>
-                    </div>
-                </label>
+
             </div>
         </div>
         <div class="row">
-            <div class="col">content</div>
+            <div class="col">
+                <div class="card">
+                    <div class="card-header">
+                        <label class="toggle-switch">
+                            <input type="checkbox" id="check123">
+                            <div class="toggle-switch-background">
+                                <div class="toggle-switch-handle"></div>
+                            </div>
+                        </label>
+                    </div>
+                    <div class="card-body">
+                        Temparature
+                    </div>
+                </div>
+            </div>
         </div>
 @endsection
 @section('script')
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
