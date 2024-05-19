6d33a9273d3d03087dbc86e9d6ebe6d0ed33e1e6
ttnhan2121 <ttnhan1212@hotmail.com>
2024-05-19 23:45:30 +0700

    123

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,9 +6,22 @@
                 <span class="material-symbols-outlined fs-1 p-2" >
                 account_circle
                 </span>
+                <label class="toggle-switch">
+                    <input type="checkbox" id="check123">
+                    <div class="toggle-switch-background">
+                        <div class="toggle-switch-handle"></div>
+                    </div>
+                </label>
             </div>
         </div>
         <div class="row">
             <div class="col">content</div>
         </div>
+@endsection
+@section('script')
+    <script>
+        $("#check123").change(function(){
+            console.log($("#check123").is(":checked"));
+        });
+    </script>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,7 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
     <style>
 
@@ -53,4 +54,7 @@
         </div>
     </div>
 </body>
+<script>
+</script>
+@yield('script')
 </html>
